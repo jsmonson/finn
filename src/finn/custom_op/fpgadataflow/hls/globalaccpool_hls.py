ef2ee0d3115838d732426a0c931e399a184b2944
auphelia <jakobapk@web.de>
2025-07-21 09:43:41 +0100

    [CustomOps] Cleanup empty line in HLS layers

--- src/finn/custom_op/fpgadataflow/hls/globalaccpool_hls.py
+++ src/finn/custom_op/fpgadataflow/hls/globalaccpool_hls.py
@@ -26,7 +26,6 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-
 from finn.custom_op.fpgadataflow.globalaccpool import GlobalAccPool
 from finn.custom_op.fpgadataflow.hlsbackend import HLSBackend
 
